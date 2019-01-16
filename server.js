822f3cb64a083c5de3ff3809948b4f5f87c4434b
alivls <alivls@yandex.ru>
2019-01-16 12:14:56 +0500

    filter duplicate array

--- server.js
+++ server.js
@@ -2,18 +2,32 @@ const express = require('express');
 
 const app = express();
 
+const cars = [
+  'Audi',
+  'Volga',
+  'BMW',
+];
+
+// eslint-disable-next-line func-names
+const filter = function () {
+  const tmp = {};
+  // eslint-disable-next-line no-return-assign
+  return cars.filter(a => (a in tmp ? 0 : tmp[a] = 1));
+};
+
 app.get('/', (req, res) => {
-  res.send('<center>Hello world</center> ');
+  const car = filter(cars);
+  res.send(car.join('<br>'));
 });
 
-// больше не хуйни
-app.get('/hello/:name', (req, res) => {
+app.get('/cars/add/:name', (req, res) => {
   const {
     name,
   } = req.params;
-  res.send(`<center>Hello ${name}</center> `);
+  cars.push(name);
+  res.send('ok');
 });
 
 app.listen(3005 || undefined, () => {
-  console.log('Server started');
+  console.log('Server started!!!');
 });
