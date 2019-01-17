367021c41e5b84088fb23fd6461d9fed4a31c2c5
alivls <alivls@yandex.ru>
2019-01-17 12:49:30 +0500

    changes
    made changes

--- server.js
+++ server.js
@@ -7,25 +7,34 @@ const cars = [
   'Volga',
   'BMW',
 ];
-
+// filter
 // eslint-disable-next-line func-names
-const filter = function () {
-  const tmp = {};
-  // eslint-disable-next-line no-return-assign
-  return cars.filter(a => (a in tmp ? 0 : tmp[a] = 1));
-};
+// const filter = function () {
+//   const tmp = {};
+//   // eslint-disable-next-line no-return-assign
+//   return cars.filter(a => (a in tmp ? 0 : tmp[a] = 1));
+// };
+// filter
 
 app.get('/', (req, res) => {
-  const car = filter(cars);
-  res.send(car.join('<br>'));
+  res.send(cars.join('<br>'));
 });
 
 app.get('/cars/add/:name', (req, res) => {
   const {
     name,
   } = req.params;
-  cars.push(name);
-  res.send('ok');
+
+  function filters(cars, name) {
+    for (let i = 0; i < cars.length; ++i) {
+      if (cars[i] === name) {
+        return res.send('Already have');
+      }
+    }
+    res.send(`ok ${name} add`);
+    return cars.push(name);
+  }
+  filters(cars, name);
 });
 
 app.listen(3005 || undefined, () => {
